20932f2d74712419f2a079f036d6390e9b87034f
Krishnansh Agarwal <5272.krishnansh@gmail.com>
2023-06-27 06:35:14 +0530

    configured nginx in docker

---
+++ likes/batchupdater.go
@@ -0,0 +1,144 @@
+package likes
+
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+	"sync"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/spf13/viper"
+)
+
+type LikeCountBuffer struct {
+	mu sync.Mutex
+	hmap map[int]int
+}
+
+func (likeCountBuffer *LikeCountBuffer) updatelikeCountBuffer(message amqp.Delivery) {
+	var voteRequest newVoteRequest
+	err := gob.NewDecoder(bytes.NewReader(message.Body)).Decode(&voteRequest)
+	if err != nil {
+		log.Println("Error in decoding vote request struct")
+		return
+	}
+	likeCountBuffer.mu.Lock()
+	defer likeCountBuffer.mu.Unlock()
+	likeCountBuffer.hmap[int(voteRequest.PostID)] = 0
+	likeCountBuffer.hmap[int(voteRequest.PostID)] = likeCountBuffer.hmap[int(voteRequest.PostID)] + int(voteRequest.VoteType)
+}
+
+func openBatchUpdaterQueue() (<-chan amqp.Delivery, error) {
+	exchangeName := viper.GetString("MQ.EXCHANGE")
+	err := ch.Publisher.ExchangeDeclare(
+		exchangeName, // name
+		"fanout",     // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	if err != nil {
+		log.Printf("Error in declaring exchange: %s", err)
+		return nil, err
+	}
+	q, err := ch.BatchUpdater.QueueDeclare(
+		viper.GetString("MQ.BATCHUPDATERQUEUE"), // name
+		true,                                    // durable
+		false,                                   // delete when unused
+		false,                                   // exclusive
+		false,                                   // no-wait
+		nil,                                     // arguments
+	)
+	if err != nil {
+		log.Printf("Error in declaring updater queue: %s", err)
+		return nil, err
+	}
+	err = ch.BatchUpdater.QueueBind(
+		q.Name,       // queue name
+		"",           // routing key
+		exchangeName, // exchange
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Printf("Error in binding updater queue: %s", err)
+		return nil, err
+	}
+	msgs, err := ch.BatchUpdater.Consume(
+		q.Name,          // queue
+		"batch_updater", // consumer
+		false,           // auto-ack
+		false,           // exclusive
+		false,           // no-local
+		false,           // no-wait
+		nil,             // args
+	)
+	if err != nil {
+		log.Printf("Error in registering updater consumer: %s", err)
+		return nil, err
+	}
+
+	return msgs, nil
+}
+
+func batchUpdater() {
+	defer ch.BatchUpdater.Close()
+	processingInterval := time.Duration(viper.GetInt("MQ.PROCESSING_INTERVAL")) * time.Second
+	timer := time.NewTicker(processingInterval)
+	
+
+	var likeCountBuffer LikeCountBuffer
+	likeCountBuffer.hmap = make(map[int]int)
+	var buffer []amqp.Delivery
+
+	msgs, err := openBatchUpdaterQueue()
+	if err != nil {
+		return
+	}
+
+	defer processBufferedMessages(buffer,&likeCountBuffer)
+	defer acknowledgeMessages(buffer)
+	for {
+		select {
+		case message := <-msgs:
+			likeCountBuffer.updatelikeCountBuffer(message)
+			buffer = append(buffer, message)
+		case <-timer.C:
+			processBufferedMessages(buffer,&likeCountBuffer)
+			acknowledgeMessages(buffer)
+			buffer = nil
+		}
+	}
+}
+
+func processBufferedMessages(buffer []amqp.Delivery, likeCountBuffer *LikeCountBuffer) {
+	if len(buffer) == 0 {
+		return
+	}
+	log.Println("Processing batch:")
+	likeCountBuffer.mu.Lock()
+	defer likeCountBuffer.mu.Unlock()
+	for _, msg := range buffer {
+		var voteRequest newVoteRequest
+		err := gob.NewDecoder(bytes.NewReader(msg.Body)).Decode(&voteRequest)
+		if err != nil {
+			log.Println("Error in decoding vote request struct")
+			continue
+		}
+		log.Print("Message: ", voteRequest)
+		// updateBatchLikeCount(voteRequest.)
+	}
+	likeCountBuffer.hmap = make(map[int]int)
+}
+
+func acknowledgeMessages(buffer []amqp.Delivery) {
+	if len(buffer) == 0 {
+		return
+	}
+	for _, msg := range buffer {
+		msg.Ack(false)
+	}
+}
--- likes/updater.go
+++ likes/updater.go
@@ -4,19 +4,10 @@ import (
 	"bytes"
 	"encoding/gob"
 	"log"
-	"sync"
 	"time"
 
-	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/spf13/viper"
 )
-
-type PostLikeCountBuffer struct {
-	mu sync.Mutex
-	hmap map[int]int
-}
-
-var postLikeCountBuffer PostLikeCountBuffer
 
 func updater() {
 	defer ch.Updater.Close()
@@ -101,113 +92,5 @@ func worker(voteRequestsCh chan newVoteRequest, id int) {
 	for voteRequest := range voteRequestsCh {
 		log.Println("worker",id, ":" , voteRequest)
 		time.Sleep(10)
-	}
-}
-
-func batchUpdater() {
-	defer ch.BatchUpdater.Close()
-	exchangeName := viper.GetString("MQ.EXCHANGE")
-	err := ch.Publisher.ExchangeDeclare(
-		exchangeName, // name
-		"fanout",     // type
-		true,         // durable
-		false,        // auto-deleted
-		false,        // internal
-		false,        // no-wait
-		nil,          // arguments
-	)
-	if err != nil {
-		log.Printf("Error in declaring exchange: %s", err)
-		return
-	}
-	q, err := ch.BatchUpdater.QueueDeclare(
-		viper.GetString("MQ.BATCHUPDATERQUEUE"), // name
-		true,                                    // durable
-		false,                                   // delete when unused
-		false,                                   // exclusive
-		false,                                   // no-wait
-		nil,                                     // arguments
-	)
-	if err != nil {
-		log.Printf("Error in declaring updater queue: %s", err)
-		return
-	}
-	err = ch.BatchUpdater.QueueBind(
-		q.Name,       // queue name
-		"",           // routing key
-		exchangeName, // exchange
-		false,
-		nil,
-	)
-	if err != nil {
-		log.Printf("Error in binding updater queue: %s", err)
-		return
-	}
-	msgs, err := ch.BatchUpdater.Consume(
-		q.Name,          // queue
-		"batch_updater", // consumer
-		false,           // auto-ack
-		false,           // exclusive
-		false,           // no-local
-		false,           // no-wait
-		nil,             // args
-	)
-	if err != nil {
-		log.Printf("Error in registering updater consumer: %s", err)
-		return
-	}
-
-	processingInterval := time.Duration(viper.GetInt("MQ.PROCESSING_INTERVAL")) * time.Second
-	timer := time.NewTicker(processingInterval)
-	var buffer []amqp.Delivery
-
-	for {
-		select {
-		case message := <-msgs:
-			updatePostLikeCountBuffer(message)
-		case <-timer.C:
-			// postLikeCountBuffer.mu.Lock()
-			processBufferedMessages(buffer)
-			acknowledgeMessages(buffer)
-			buffer = nil
-			postLikeCountBuffer.hmap = nil
-			// postLikeCountBuffer.mu.Unlock()
-		}
-	}
-}
-
-func updatePostLikeCountBuffer(message amqp.Delivery) {
-	var voteRequest newVoteRequest
-	err := gob.NewDecoder(bytes.NewReader(message.Body)).Decode(&voteRequest)
-	if err != nil {
-		log.Println("Error in decoding vote request struct")
-		return
-	}
-	// postLikeCountBuffer.mu.Lock()
-	// defer postLikeCountBuffer.mu.Unlock()
-	// postLikeCountBuffer.hmap[int(voteRequest.PostID)] += int(voteRequest.VoteType)
-}
-
-func processBufferedMessages(buffer []amqp.Delivery) {
-	if len(buffer) == 0 {
-		return
-	}
-	log.Println("Processing batch:")
-	for _, msg := range buffer {
-		var voteRequest newVoteRequest
-		err := gob.NewDecoder(bytes.NewReader(msg.Body)).Decode(&voteRequest)
-		if err != nil {
-			log.Println("Error in decoding vote request struct")
-			continue
-		}
-		log.Print("Message: ", voteRequest)
-		// updateBatchLikeCount(voteRequest.)
-	}
-	log.Println()
-}
-
-func acknowledgeMessages(buffer []amqp.Delivery) {
-	for _, msg := range buffer {
-		msg.Ack(false)
 	}
 }
